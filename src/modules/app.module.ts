d9c9cd66006ed3cb4d7f284d02eb1dee5746def1
Amrullah Farook <amrullahfahimfarook@gmail.com>
2023-10-06 21:49:02 +0530

    create user resource

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { GoogleStrategy } from './auth/strategies/google.strategy';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
   controllers: [],
   providers: [GoogleStrategy],
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   googleLogin(req) {
+    // todo: throw notFoundException
     if (!req.user) {
       return 'No user from google';
     }
 
+    // todo: create return type for google oauth login
     return {
       message: 'User information from google',
       user: req.user,
